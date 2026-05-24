dfbb6bacd15d2f37377dbbf258deef812b59295e
Newtybei <zhengshihong66y@gmail.com>
2026-05-24 09:22:53 +0800

    Initial Commit

---
+++ LapianBao/LapianBaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapianBaoApp.swift
+//  LapianBao
+//
+//  Created by 郑士泓 on 2026/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LapianBaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
